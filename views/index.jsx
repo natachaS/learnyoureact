d6f2f49206435480c27ba3349e4d8691fb8c3b9f
natachaS <natacha.springer@gmail.com>
2015-08-06 11:09:26 -0400

    passed STATE

--- views/index.jsx
+++ views/index.jsx
@@ -13,6 +13,7 @@ var TodoBox = React.createClass({
 });
 
 var TodoList = React.createClass({
+
   render: function() {
     return (
       <div className = "todoList">
@@ -31,9 +32,19 @@ var Todo = React.createClass({
   propTypes: {
     title: React.PropTypes.string.isRequired   
   },
+  getInitialState: function(){
+    return {
+      checked : false
+    };
+  },
+
+  handleChange: function(){
+
+  },
   render: function() {
     return (
       <tr>
+        <td style={{border: "1px solid black"}}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
         <td style={{border: "1px solid black"}}>{this.props.title}</td>
         <td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
